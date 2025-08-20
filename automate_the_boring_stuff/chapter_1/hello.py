028c4b4c91e3ae09a66f5b2e24ce78714acd634d
Brandon Duff <165906017+Bmoney42@users.noreply.github.com>
2025-08-20 06:35:12 -0500

    Create hello.py

---
+++ automate_the_boring_stuff/chapter_1/hello.py
@@ -0,0 +1,11 @@
+# Write your code here :-)
+print('Hello, world!')
+print('What is your name?') #asks for their name
+my_name  = input('>')
+print('It is good to meet you, ' + my_name)
+print('the length of your name is:')
+print(len(my_name))
+print('What is your age?') #ask for their age
+my_age = input('>')
+print('you will be ' + str(int(my_age) + 1) + ' in a year.')
+
