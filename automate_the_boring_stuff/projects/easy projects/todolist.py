30ac5dc09d744bfe3c066086020bfa72d24f2a81
Brandon Duff <165906017+Bmoney42@users.noreply.github.com>
2025-08-29 16:40:51 -0500

    updated logic for todo list

--- automate_the_boring_stuff/projects/easy projects/todolist.py
+++ automate_the_boring_stuff/projects/easy projects/todolist.py
@@ -1,25 +1,34 @@
-#
+import sys
 
 todo = []
 
-While True:
-  print("\nWhat Would You Like To Do")
-  print("1. Add Task")
-  print("2. Remove Task")
-  print("3. Show Tasks")
-  print("4. Quit")
-  choice = input("Enter Your Choice")
+while True:
+    print("\nWhat Would You Like To Do")
+    print("1. Add Task")
+    print("2. Remove Task")
+    print("3. Show Tasks")
+    print("4. Quit")
+    print(
+    user_input = input("Enter Your Choice ")
 
-if choice == 1:
-  task = input("Enter New Task")
-  todo.append(task)
-  print(f"Added: {task}")
+    if user_input.isdigit():
+        choice = int(user_input)
 
-if choice == 2:
-  task = input("Which Task Do You Want To Remove")
-  todo.remove(task)
-  print(f"Remove: {task}")
+        if choice == 1:
+            task = input("Enter New Task ")
+            todo.append(task)
+            print(f"Added: {task}")
+            
+        elif choice == 2:
+            task = input("Which Task Do You Want To Remove ")
+            todo.remove(task)
+            print(f"Remove: {task}")
+            
+        elif choice == 3:
+            for t in todo: 
+                print(t)
+        elif choice == 4:
+            sys.exit()
 
-
-
+            
 
