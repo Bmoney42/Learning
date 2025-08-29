99cc0304dd940871e9a4912d50c0b6b7e624b4b0
Brandon Duff <165906017+Bmoney42@users.noreply.github.com>
2025-08-29 16:49:56 -0500

    clearring screen on option

--- automate_the_boring_stuff/projects/easy projects/todolist.py
+++ automate_the_boring_stuff/projects/easy projects/todolist.py
@@ -1,34 +1,36 @@
-import sys
-
 todo = []
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')   # Windows vs Mac/Linux
 
 while True:
     print("\nWhat Would You Like To Do")
     print("1. Add Task")
     print("2. Remove Task")
     print("3. Show Tasks")
-    print("4. Quit")
-    print(
-    user_input = input("Enter Your Choice ")
+    print("4. Quit\n")
+    user_input = input("Enter Your Choice: ")
 
     if user_input.isdigit():
         choice = int(user_input)
 
         if choice == 1:
+            clear_screen()
             task = input("Enter New Task ")
             todo.append(task)
             print(f"Added: {task}")
             
         elif choice == 2:
+            clear_screen()
             task = input("Which Task Do You Want To Remove ")
             todo.remove(task)
             print(f"Remove: {task}")
             
         elif choice == 3:
+            clear_screen()
             for t in todo: 
                 print(t)
+                
         elif choice == 4:
             sys.exit()
-
             
-
